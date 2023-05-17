72550e4fa454c03c3aeb4ce7bedac4be6743fccf
Thomas Carlier <carlier.thomas@gmail.com>
2023-05-17 14:48:10 +1000

    Update Summary and Description for DeleteCustomerSummary

--- Customers.Api/Summaries/DeleteCustomerSummary.cs
+++ Customers.Api/Summaries/DeleteCustomerSummary.cs
@@ -7,8 +7,8 @@ public class DeleteCustomerSummary : Summary<DeleteCustomerEndpoint>
 {
     public DeleteCustomerSummary()
     {
-        Summary = "Deleted a customer the system";
-        Description = "Deleted a customer the system";
+        Summary = "Deletes a customer from the system";
+        Description = "Deletes a customer from the system";
         Response(204, "The customer was deleted successfully");
         Response(404, "The customer was not found in the system");
     }
